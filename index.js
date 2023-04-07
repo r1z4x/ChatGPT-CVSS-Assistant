28257f65b4d214b4adba6037f7de7bf87f4f3ff7
r1z4x <rizax@tuta.io>
2023-04-07 22:37:49 +0300

    fix-7

--- index.js
+++ index.js
@@ -13,22 +13,36 @@ app.post('/ask', async (req, res) => {
     return;
   }
   try {
-    
+
     const { Configuration, OpenAIApi } = require("openai");
     const configuration = new Configuration({
       apiKey: process.env.OPENAI_API_KEY,
     });
     const openai = new OpenAIApi(configuration);
-    const response = await openai.createCompletion({
-      model: "text-davinci-003",
-      prompt: prompt,
-      temperature: 0,
-      max_tokens: 7,
+    const response = await openai.createChatCompletion({
+      model: "gpt-3.5-turbo",
+      messages: [
+        { role: "user", content: "I am a cybersecurity expert, and you are my top consultant in the field of cybersecurity. You have extensive experience in CVSS calculations and scenarios." },
+        { role: "user", content: "I want you to create a JSON structure in Turkish that includes all the CVSS values for the scenario I provided to you, along with explanatory points for each CVSS category related to the mentioned scenario. No additional explanation is needed." },
+        { role: "user", content: "Scenario: " + prompt },
+      ],
+      temperature: 0.7,
+      max_tokens: 2000
     });
 
+    let chatbotJSON;
+    if (typeof response.data.choices != 'undefined' && response.data.choices.length > 0) {
+      try {
+        const chatbotAnswer = response.data.choices[0].message.content.trim();
+        chatbotJSON = JSON.parse(chatbotAnswer);
+      } catch (error) {
+        console.error('Chatbot response could not be parsed:', error);
+      }
+    } else {
+      console.error('Unable to get response from the chatbot.');
+    }
  
-    const chatbotAnswer = response.data.choices[0].text.trim();
-    res.json({ answer: chatbotAnswer });
+    res.json({ answer: chatbotJSON });
   } catch (error) {
     console.error(error);
     res.status(500).json({ error: 'API request failed' });
